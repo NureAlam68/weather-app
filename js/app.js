bd9390d46751aa00efc56650d668fa2b95bb3c88
NureAlam68 <nurealam151068@gmail.com>
2024-10-08 20:20:24 +0600

    finish update city and weather

--- js/app.js
+++ js/app.js
@@ -9,7 +9,12 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const res = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await res.json();
+
+    if (res.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await res.json();
 
     console.log(data);
 
@@ -37,6 +42,10 @@ async function checkWeather(city) {
     }
     else if (data.weather[0].main == "Snow") {
         weatherIcon.src = "assets/snow.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
     }
 };
 
